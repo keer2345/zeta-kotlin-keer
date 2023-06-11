32f1628dbcb686216410188902cd275e1b85adff
keer2345 <keer2345@gmail.com>
2023-06-11 22:49:33 +0800

    init

---
+++ test/kotlin/com/keer/zeta/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.keer.zeta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
